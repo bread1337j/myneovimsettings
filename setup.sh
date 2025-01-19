0fe8bcd96d6303a92c572af1693fc75e2f10618e
bread <131266168+bread1337j@users.noreply.github.com>
2025-01-19 13:10:59 -0500

    forgot to add the plugin manager but added it now

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+
+echo "Installing the plugin manager"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 if test -f ./.vimrc; then 
 	echo "vimrc found"
 	mv .vimrc $HOME
