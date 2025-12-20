97189f42d49401d1ef354a3c3a346e3838cd0f91
bread1337j <131266168+bread1337j@users.noreply.github.com>
2025-12-19 19:49:35 -0500

    fixed clangd not being able to see an include dir

--- init.lua
+++ init.lua
@@ -15,6 +15,8 @@ Plug('kyazdani42/nvim-tree.lua')
 Plug('kyazdani42/nvim-web-devicons')
 Plug('romgrk/barbar.nvim')
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+Plug('xiyaowong/transparent.nvim')
+Plug('lervag/vimtex')
 
 vim.call('plug#end')
 
@@ -27,5 +29,45 @@ require("common")
 --dofile("theme.lua")
 require("vimtree")
 require("barbar")
+
+vim.lsp.config('*', {
+	root_markers = {'.git'}, 
+})
+
+-- Define the configuration for clangd
+vim.lsp.config.clangd = {
+	cmd = {
+		'clangd',
+		'--clang-tidy',
+		'--background-index',
+		'--offset-encoding=utf-16', -- Note: utf-16 is often preferred by clangd for Neovim
+		'--enable-config',          -- This enables .clangd file support
+		'--background-index',
+		'--query-driver=/usr/bin/gcc',
+	},
+	root_dir = function(fname)
+		return vim.fs.dirname(vim.fs.find({'.clangd', '.git', 'compile_commands.json'}, { upward = true, path = fname })[1])
+	end,
+	-- Adding .clangd here ensures the LSP starts when it sees that file
+	root_markers = { '.clangd', 'compile_commands.json', '.git' },
+	filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+	capabilities = {
+		textDocument = {
+			completion = {
+				completionItem = {
+					snippetSupport = true,
+				}
+			}
+		}
+	}
+}
+
+-- Then, ensure you actually start/enable the server
+vim.lsp.enable('clangd')
+
+
+
+
+vim.lsp.enable('clangd')
 
 vim.cmd("wincmd p")
